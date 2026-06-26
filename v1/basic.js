b740e4bf70625852e6196f59742f02ff93b1df62
lakshya <lakshyaparik99@gmail.com>
2026-06-26 20:23:05 +0530

    basics understandings

--- v1/basic.js
+++ v1/basic.js
@@ -1,2 +1,45 @@
-var a = 1
-console.log(a)
+// a=5;
+// console.log(a)  //5
+// var a ;  
+
+// let a = 5;
+// let b = '10'
+// console.log(a+b); // 510  here concatination performed because of string and number
+
+//swap two numbers 3 methods
+
+//with variable
+
+// let a = 5;
+// let b = 10;
+// let temp = a;
+// a = b;
+// b = temp;
+//console.log(a, b); // 10 5
+
+
+//without variable
+
+// let a = 5 ;
+// let b = 10;
+
+// a = a+b; //a=15
+// b = a-b; //b=5
+// a = a-b; //a=10
+// console.log(a, b); // 10 5
+
+
+//with destructuring
+
+// let a = 5;
+// let b = 10;
+
+// [a, b] = [b, a];
+// console.log(a, b); // 10 5
+
+//operator in js
+
+// console.log(7/2); // 3.5
+// console.log(Math.floor(7/2)); // 3   we should use math.floor in everywhere 
+
+//
