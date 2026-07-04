9e617b096fe027579435e9b358d62d611af04019
lakshya <lakshyaparik99@gmail.com>
2026-07-04 19:04:46 +0530

    switch case

---
+++ v2/switch-case/switch.js
@@ -0,0 +1,27 @@
+let val =3;
+
+switch(val){
+  case 1:
+    case 2:
+      case 3:
+        console.log("The value is 1,2 or 3");
+        break;
+
+  case 4:
+    console.log("The value is 4");
+    break;
+  default:
+    console.log("The value is not 1,2,3 or 4");
+}
+
+let abc = 0.1 + 0.2;
+switch(abc){
+  case 0.3:
+    console.log("The value is 0.3");
+    break;
+  case 0.5:
+    console.log("The value is 0.5");
+    break;
+  default:
+    console.log("The value is not 0.3 or 0.5");
+}
