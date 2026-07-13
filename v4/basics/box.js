2ff38d298dbe5fc5f100fcfe567bb921afde0de3
lakshya <lakshyaparik99@gmail.com>
2026-07-13 16:13:42 +0530

    another pattern

--- v4/basics/box.js
+++ v4/basics/box.js
@@ -33,8 +33,25 @@ for(let i=1; i<=n; i++){
 for(let i=1; i<=n;i++){
   for(j=1;j<=i;j++){
     let char = 'A';
-    process.stdout.write(char + " ");
+    //process.stdout.write(char + " ");
     ++char;
   }
   console.log(); // Move to the next line after each row
+}
+
+
+
+// A 
+// A B
+// A B C
+// A B C D
+
+for(let i=1; i<=n; i++){
+  let char =  65;
+  for(let j=1;j<=i;j++){
+    process.stdout.write(String.fromCharCode(char)+ " ");
+    char++;
+  }
+  console.log();
+  
 }
