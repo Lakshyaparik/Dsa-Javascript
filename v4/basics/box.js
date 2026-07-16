6d35698f5a5803da36172f5ef13b435676013410
lakshya <lakshyaparik99@gmail.com>
2026-07-16 18:30:51 +0530

    pattern completed

--- v4/basics/box.js
+++ v4/basics/box.js
@@ -49,8 +49,98 @@ for(let i=1; i<=n;i++){
 for(let i=1; i<=n; i++){
   let char =  65;
   for(let j=1;j<=i;j++){
-    process.stdout.write(String.fromCharCode(char)+ " ");
+    //process.stdout.write(String.fromCharCode(char)+ " ");
     char++;
+  }
+  console.log();
+  
+}
+
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+for(let i=1;i<=n;i++){
+  for(let j=1;j<=n-i+1;j++){
+   // process.stdout.write("* ");
+  }
+  console.log();
+}
+
+
+// - - - - *
+// - - - * *
+// - - * * *
+// - * * * *
+// * * * * *
+
+
+for(let i = 1; i<=n; i++){
+
+  for(let j=1;j<=n-i; j++){
+    process.stdout.write("  ")
+  }
+
+  for(let j=1; j <= i; j++){
+   // process.stdout.write('* ')
+  }
+
+  console.log();
+  
+}
+
+
+// *                 * 
+//   *             *   
+//     *         *     
+//       *     *       
+//         * *         
+//         * *         
+//       *     *       
+//     *         *     
+//   *             *   
+// *                 * 
+
+
+
+for(let i=1; i<=n;i++){
+  for(let j=1;j<=n;j++){
+    if(i==j || i+j == n+1){
+     // process.stdout.write("* ")
+    }
+    else{
+     // process.stdout.write("  ")
+    }
+  }
+  console.log();
+  
+}
+
+
+// *                                   *   
+//   *                               *     
+//     *                           *       
+//       *                       *         
+//         *                   *           
+//           *               *             
+//             *           *               
+//               *       *                 
+//                 *   *                   
+//                   *                  
+
+
+for(let i=1; i<=n; i++){
+  for(let j=1;j<=n*2; j++){
+    if( i==j || i+j == n*2 )
+    {
+      process.stdout.write('* ')
+    }
+    else{
+      process.stdout.write('  ')
+    }
   }
   console.log();
   
